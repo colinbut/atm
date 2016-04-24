e5dec7c3e1cb67bc5ac0105529f5ecb716e8931d
Colin But <colinbut@users.noreply.github.com>
2016-04-24 11:10:46 +0100

    Renamed App to appropriate class name

---
+++ src/main/java/com/mycompany/atm/AtmApplication.java
@@ -0,0 +1,20 @@
+package com.mycompany.atm;
+
+/**
+ * ATM
+ *
+ * @author colin
+ *
+ */
+public class AtmApplication {
+
+
+    /**
+     * Main entry
+     *
+     * @param args
+     */
+    public static void main( String[] args ) {
+        System.out.println("ATM");
+    }
+}
