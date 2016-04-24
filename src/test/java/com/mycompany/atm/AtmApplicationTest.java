351a1682d6087bcb5f63a13583358af270d75558
Colin But <colinbut@users.noreply.github.com>
2016-04-24 11:12:18 +0100

    Removed generated test code

--- src/test/java/com/mycompany/atm/AtmApplicationTest.java
+++ src/test/java/com/mycompany/atm/AtmApplicationTest.java
@@ -1,38 +1,11 @@
 package com.mycompany.atm;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
 
 /**
  * Unit test for simple AtmApplication.
+ *
+ * @author colin
  */
-public class AtmApplicationTest
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AtmApplicationTest(String testName )
-    {
-        super( testName );
-    }
+public class AtmApplicationTest {
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AtmApplicationTest.class );
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
 }
