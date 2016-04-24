3c1a5c7541fb661eda1190e5b73dfef925eedf39
Colin But <colinbut@users.noreply.github.com>
2016-04-24 11:24:24 +0100

    Bank app

---
+++ src/main/java/com/mycompany/atm/bank/model/BankAccount.java
@@ -0,0 +1,9 @@
+/*
+ * |-------------------------------------------------
+ * | Copyright © 2016 Colin But. All rights reserved.
+ * |-------------------------------------------------
+ */
+package com.mycompany.atm.bank.model;
+
+public class BankAccount {
+}
