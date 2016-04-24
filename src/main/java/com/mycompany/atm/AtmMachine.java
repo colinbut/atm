ad72f099b0dcf365c23552d61c0fbc639b971909
Colin But <colinbut@users.noreply.github.com>
2016-04-24 11:14:06 +0100

    Added maven compiler plugin

---
+++ src/main/java/com/mycompany/atm/AtmMachine.java
@@ -0,0 +1,19 @@
+/*
+ * |-------------------------------------------------
+ * | Copyright © 2016 Colin But. All rights reserved.
+ * |-------------------------------------------------
+ */
+package com.mycompany.atm;
+
+/**
+ * Represents an Automated Teller Machine
+ *
+ * @author colin
+ */
+public class AtmMachine implements Runnable{
+
+    @Override
+    public void run() {
+
+    }
+}
