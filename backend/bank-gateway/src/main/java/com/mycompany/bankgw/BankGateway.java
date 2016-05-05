9909951c09b377ba966f417387a07b69332417c6
Colin But <colinbut@users.noreply.github.com>
2016-05-05 23:33:23 +0100

    removed generated source files from maven generate

---
+++ backend/bank-gateway/src/main/java/com/mycompany/bankgw/BankGateway.java
@@ -0,0 +1,17 @@
+package com.mycompany.bankgw;
+
+/**
+ * Bank Application Adapter
+ *
+ */
+public class BankGateway {
+
+    /**
+     * Main entry
+     *
+     * @param args
+     */
+    public static void main( String[] args ) {
+        System.out.println("Bank Application Adapter");
+    }
+}
